9c4505b8c9bca7203e9350fcf38ee9cfb4acf390
Asif Kabani <asifkabani@gmail.com>
2018-09-05 12:57:29 -0500

    Added styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'JavaScript Designer',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components'
+  ],
 }
