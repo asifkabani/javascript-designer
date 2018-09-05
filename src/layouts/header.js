1b6cf926d357670f703f223f17156fa5b61d92a0
Asif Kabani <asifkabani@gmail.com>
2018-09-05 15:36:44 -0500

    Added styled components

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -2,63 +2,50 @@ import React from 'react'
 import Link from 'gatsby-link'
 import styled from 'styled-components';
 
+const Header = styled.header`
+  background-color: #FFE200;
+  text-align: center;
+  color: #E83F6F;
+  font-family: "Josefin Sans";
+  padding-top: 20px;
+`;
 
+const PinkBar = styled.hr`
+  background-color: #E83F6F;
+  width: 50%;
+  margin-left: 25%;
+  height: 5px;
+  border-radius: 30px;
+`;
 
-// const Header = ({ siteTitle }) => {
-//   return (
-//     <header className={styles.header}>
-//       <h1>
-//         JavaScript
-//         <span>
-//           Designer
-//         </span>
-//       </h1>
-//     </header>
-//   );
-// }
+const Logo = () => (
+  <h1>
+      <Link to="/" style={{
+        textDecoration: 'none',
+        color: '#E83F6F',
+        fontFamily: 'Josefin Sans',
+        fontSize: 50
+      }}>
+        JavaScript
+        <span style={{
+          display: 'block',
+          fontFamily: 'Mr Dafoe',
+          fontSize: 50,
+          color: 'white',
+          fontWeight: 100,
+          lineHeight: 0.5,
+          paddingBottom: 20
+        }}>
+          Designer
+        </span>
+      </Link>
+    </h1>
+);
 
 export default () => (
   <Header>
-    <h1>
-      JavaScript
-      <span>
-        Designer
-      </span>
-    </h1>
+    <Logo />
     <p>A Front End Developer's Journey</p>
+    <PinkBar />
   </Header>
 );
-
-// const Header = ({ siteTitle }) => (
-//   <header className={styles.header}>
-//     <h1>
-//       {siteTitle}
-//     </h1>
-//   </header>
-  // <div
-  //   style={{
-  //     background: 'midnightblue',
-  //     marginBottom: '1.45rem',
-  //   }}
-  // >
-  //   <div
-  //     style={{
-  //       margin: '0 auto',
-  //       maxWidth: 960,
-  //       padding: '1.45rem 1.0875rem',
-  //     }}
-  //   >
-  //     <h1 style={{ margin: 0 }}>
-  //       <Link
-  //         to="/"
-  //         style={{
-  //           color: 'white',
-  //           textDecoration: 'none',
-  //         }}
-  //       >
-  //         {siteTitle}
-  //       </Link>
-  //     </h1>
-  //   </div>
-  // </div>
-// )
