96369e58a6dc128df0fb7c7465d5e9d8b5eef694
Asif Kabani <asifkabani@gmail.com>
2018-09-05 13:03:20 -0500

    Added files

---
+++ src/layouts/header.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import styled from 'styled-components';
+
+
+
+// const Header = ({ siteTitle }) => {
+//   return (
+//     <header className={styles.header}>
+//       <h1>
+//         JavaScript
+//         <span>
+//           Designer
+//         </span>
+//       </h1>
+//     </header>
+//   );
+// }
+
+export default () => (
+  <Header>
+    <h1>
+      JavaScript
+      <span>
+        Designer
+      </span>
+    </h1>
+    <p>A Front End Developer's Journey</p>
+  </Header>
+);
+
+// const Header = ({ siteTitle }) => (
+//   <header className={styles.header}>
+//     <h1>
+//       {siteTitle}
+//     </h1>
+//   </header>
+  // <div
+  //   style={{
+  //     background: 'midnightblue',
+  //     marginBottom: '1.45rem',
+  //   }}
+  // >
+  //   <div
+  //     style={{
+  //       margin: '0 auto',
+  //       maxWidth: 960,
+  //       padding: '1.45rem 1.0875rem',
+  //     }}
+  //   >
+  //     <h1 style={{ margin: 0 }}>
+  //       <Link
+  //         to="/"
+  //         style={{
+  //           color: 'white',
+  //           textDecoration: 'none',
+  //         }}
+  //       >
+  //         {siteTitle}
+  //       </Link>
+  //     </h1>
+  //   </div>
+  // </div>
+// )
