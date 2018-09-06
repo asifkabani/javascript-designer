88c54045a9c80ba7cc7f8e202876cbda6e4feda9
Asif Kabani <asifkabani@gmail.com>
2018-09-06 00:33:19 -0500

    Updated files

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -15,7 +15,7 @@ const Layout = ({ children, data }) => (
       ]}
     />
     <Header />
-    <div
+    <section
       style={{
         margin: '0 auto',
         maxWidth: 960,
@@ -24,7 +24,7 @@ const Layout = ({ children, data }) => (
       }}
     >
       {children()}
-    </div>
+    </section>
     <Footer />
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,33 +2,61 @@ import React from "react";
 import Link from "gatsby-link";
 import styled from 'styled-components';
 
-const Post = styled.article`
-  background-color: 'red';
+const Post = styled.div`
+  background-color: #FFFFFF;
+  border-radius: 15px;
+  box-shadow: 0px 10px 30px 0px rgba(0, 0, 0, 0.1);
+  padding: 50px;
+  font-family: 'Josefin Sans';
+`;
+
+const Category = styled.p`
+  color: #BABABA;
+  font-size: 18px;
+  font-weight: 400;
+  display: inline;
+`;
+
+const Date = styled.p`
+  color: #999;
+  display: inline;
+`;
+
+const Headline = styled.h2`
+  color: #676767;
+  font-family: 'Josefin Sans';
+`;
+
+const Excerpt = styled.p`
+  color: #999;
 `;
 
 export default ({ data }) => {
   return (
-    <div style={{
-      backgroundColor: 'white',
-      borderRadius: '15px',
-      boxShadow: '0px 10px 30px 0px rgba(0, 0, 0, 0.1)',
-      padding: 50
-    }}>
+    <main style={{ display: 'grid', gridGap: '30px', gridTemplateColumns: 'repeat(autofit, minmax(200px, 1fr))'}}>
       {data.allMarkdownRemark.edges.map(({ node }) =>
-        <div key={node.id}>
-          <Link to={node.fields.slug}>
-            <p>{node.frontmatter.category}</p>
-            <h3>
-              {node.frontmatter.title}{" "}
-              <span color="#BBB">— {node.frontmatter.date}</span>
-            </h3>
-            <p>
-              {node.excerpt}
-            </p>
+        <article key={node.id}>
+          <Link to={node.fields.slug} style={{ textDecoration: 'none' }}>
+            <Post>
+              <div>
+                <Category>
+                  {node.frontmatter.category}
+                </Category>
+                <Date>
+                  {node.frontmatter.date}
+                </Date>
+              </div>
+              <Headline>
+                {node.frontmatter.title}
+              </Headline>
+              <Excerpt>
+                {node.excerpt}
+              </Excerpt>
+            </Post>
           </Link>
-        </div>
+        </article>
       )}
-    </div>
+    </main>
   );
 }
 
