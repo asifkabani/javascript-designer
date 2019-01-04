1475a0acb71a65d6a50d09d716fe6f461d1e18ff
Asif Kabani <asifkabani@gmail.com>
2019-01-03 20:53:10 -0600

    Updated files

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -5,7 +5,11 @@ import { faBrain, faHeart } from '@fortawesome/free-solid-svg-icons';
 
 export default () => (
     <Footer>
-        <small style={{ display: 'inline-block', padding: '20px 0', letterSpacing: '1.3px', lineHeight: '1.5' }}>
+        <small style={{
+            display: 'inline-block',
+            padding: '20px 0',
+            lineHeight: '1.5'
+            }}>
             Made with <FontAwesomeIcon icon={faBrain} color="#ff3399" alt="Brain" /> & <FontAwesomeIcon icon={faHeart} color='red' alt="Love" /><br />
             JavaScript Designer © Copyright 2018
         </small>
