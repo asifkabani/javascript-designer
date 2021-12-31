d4c4cff07996fc1dc793a58fc5aa341aa9df856e
askabani <askabani@deloitte.com>
2021-12-31 13:20:15 -0600

    Add link to twitter in bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { useSiteMetadata } from "../hooks/useSiteMetadata";
-import authorImage from "../images/ak.jpeg";
+import React from 'react'
+import { useSiteMetadata } from '../hooks/useSiteMetadata'
+import authorImage from '../images/ak.jpeg'
 
 function Bio() {
   const {
     author: { name },
-  } = useSiteMetadata();
+  } = useSiteMetadata()
 
   return (
     <div className="flex justify-center pb-10">
@@ -15,11 +15,14 @@ function Bio() {
         alt={name}
       />
       <p className="text-white w-96">
-        Personal blog of {name}. You can follow me on Twitter or get in touch
-        with me via email.
+        Personal blog of {name}. You can follow me on{' '}
+        <a href="https://twitter.com/js_designer006" target="_blank" rel="noreferrer">
+          Twitter
+        </a>{' '}
+        or get in touch with me via email.
       </p>
     </div>
-  );
+  )
 }
 
-export default Bio;
+export default Bio
