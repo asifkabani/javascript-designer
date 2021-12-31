9495541c6aa972eb1ef13a1aa7cb245b2b97c67a
askabani <askabani@deloitte.com>
2021-12-30 21:47:36 -0600

    Add logo component

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,23 +1,11 @@
-import React from "react";
-import { useSiteMetadata } from "../hooks/useSiteMetadata";
-import { Link } from "gatsby";
+import React from 'react';
+import { Main, Secondary } from '../layouts/basecss';
 
-function Logo() {
-  const { title } = useSiteMetadata();
-  const mainText = title.split(" ")[0];
-  const secondaryText = title.split(" ")[1];
-
-  return (
-    <Link to="/">
-      <h1 className="font-josefinSans font-medium text-jsdYellow text-6xl inline-block leading-3 tracking-tighter pt-10">
-        {mainText}
-        <br />
-        <span className="font-mrDafoe text-white text-6xl inline-block pb-10">
-          {secondaryText}
-        </span>
-      </h1>
-    </Link>
-  );
-}
-
-export default Logo;
+export default () => (
+  <a href="/" style={{ textDecoration: 'none' }}>
+    <Main>
+      JavaScript<br/>
+      <Secondary secondary>Designer</Secondary>
+    </Main>
+  </a>
+);
